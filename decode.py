f812240b299faea6f2e139d70e81abab945439bd
Cristina Martin Perez <c.martinperez@hotmail.com>
2022-06-02 09:55:46 +0200

    minor changes

--- decode.py
+++ decode.py
@@ -1,63 +1,54 @@
 #!/usr/bin/env python
-#python3 decode.py trace_cathode2750V.dat
 
-from sys import argv , exit
-from numpy import array , uint32 , cumsum, roll , zeros , float32 , arange , shape , argmax , absolute , argmin , zeros_like
+"""
+Script to convert binary format to numpy arrays for data acquired with DRS4 evaluation boards.
+http://www.psi.ch/drs/evaluation-board
+http://www.psi.ch/drs/DocumentationEN/manual_rev40.pdf
+By: Jonas Rembser, Gregor Kasieczka, Dmitry Hits, Cristina Martin Perez
+"""
+
+from sys import argv, exit
+from numpy import array, uint32, cumsum, roll, zeros, float32, arange
 from struct import unpack
+import matplotlib.pyplot as plt
 
-import matplotlib
-from matplotlib import pyplot as plt
-import pandas as pd
-import os as os
+# To run: python decode.py filename.dat
 
+# Inputs
 if not len(argv) == 2:
-    print ("Wrong number of arguments!")
-    from matplotlib import pyplot as plt
-    import pandas as pd
-    import os as os
-    print("Usage: python decode.py filename.dat") 
-    print("Exiting ... ")
-    exit ()
-
-maxtime=0
-maxcounter=0
-tempchannel_v =[ ]
-valid_events =[[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ]]
-
-input_filename = argv [1]
-if not input_filename [-4:] == '.dat' :
-    print("Wrong arguments!")
+    print("Wrong number of arguments!")
     print("Usage: python decode.py filename.dat")
-    print("Exiting ... ")
-    exit ()
+    print("Exiting...")
+    exit()
 
-input_filename = input_filename [:-4] # Just take the name without the extension
-f = open(input_filename+'.dat', "rb")
+input_filename = argv[1]
+f = open(input_filename, "rb")
 
-if not os.path.exists(input_filename+"_Data"):
-    os.makedirs(input_filename+"_Data")
-
+# Board and event serial number
+board_serials = []
+event_serial = array([0], dtype=uint32)
 
 # Total number of channels and boards
-n_ch=0
-n_boards=0
+n_ch = 0
+n_boards = 0
 
-# Empty lists for time/voltage values
-channels_t = [ ]
-channels_v = [ ]
-channels_vlist =[ ]
-channels_tlist =[ ]
-timebins = [ ]
-average_channel_v =[ ]
+# Time and voltage numpy arrays (one per channel)
+channels_t = []
+channels_v = []
 
-while True :
+# List of numpy arrays to store the events and time bin information
+events = []
+timebins = []
+
+# Extract time information for each DRS4 cell
+while True:
     header = f.read(4)
     # Skip the initial time header
-    if header==b"TIME":
+    if header == b"TIME":
         continue
     elif header.startswith(b"C"):
         n_ch = n_ch + 1
-        # Create variables
+        # Create empty arrays for voltage and time
         channels_t.append(zeros(1024, dtype=float32))
         channels_v.append(zeros(1024, dtype=float32))
         # Write timebins to numpy array
@@ -65,111 +56,94 @@ while True :
 
     # Increment the number of boards when seeing a new serial number
     # and store the serial numbers in the board serial numbers vector
-    elif header . startswith (b"B#" ) :
+    elif header.startswith(b"B#"):
+        board_serial = unpack(b'H', header[2:])[0]
+        board_serials.append(board_serial)
         n_boards = n_boards + 1
+
     # End the loop if header is not CXX or a serial number
-    elif header==b"EHDR":
+    elif header == b"EHDR":
         break
 
-
-validcounter=0
+# Main loop
 current_board = 0
 tcell = 0 # current trigger cell
-t_00 = 0 # time in first cell in first channel for alignment
+t_00 = 0 # time in first cell in first channel (for alignment)
 is_new_event = True
-eventslist =[ ]
-info_string = "Reading in events measured with {0} channels on {1} board(s) ... "
-print (info_string.format(n_ch, n_boards))
-valid_event =[[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ]]
-time_valid_event =[[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ] ,[ ]]
-testmin =[ ]
-written_channels = [ ]
-current_event_n = 1
 
+print('\n')
+info_string = "Reading events measurend with {0} channels on {1} board(s)..."
+print(info_string.format(n_ch, n_boards))
 
-while True:    
-    var=0
-    delta_t =[ ]
-
+while True:
     # Start of Event
     if is_new_event:
+        event_serial[0] = unpack("I", f.read(4))[0]
+        if event_serial[0]%10 == 0:
+            print("Running on event : ", event_serial[0])
         is_new_event = False
+        # Set the timestamp, where the milliseconds need to be converted to
+        # nanoseconds to fit the function arguments
         dt_list = unpack("H"*8, f.read(16))
+
         # Fluff the serial number and read in trigger cell
         fluff = f.read(4)
         tcell = unpack('H', f.read(4)[2:])[0]
+        # Reset current board number
         current_board = 0
         continue
-    # Read the header , this is either
-    # EHDR -> finish event 
-    # C00x =-> read the data 
-    # "" -> endoffile
+
+    # Read the header, this is either
+    #  EHDR -> finish event
+    #  C00x -> read the data
+    #  ""   -> end of file
     header = f.read(4)
+
+    # Handle next board
     if header.startswith(b"B#"):
         current_board = current_board + 1
         tcell = unpack(b'H', f.read(4)[2:])[0]
         continue
+
     # End of Event
-
-    elif header==b"EHDR":
-        # Fill previous event
+    elif header == b"EHDR":
+        events.append(array(list(zip(channels_t[1],channels_v[1]))))
         is_new_event = True
-        print ("Done with event number ", current_event_n)
-        current_event_n += 1
-    
-    elif header.startswith(b"C"):
-    # the voltage info is 1024 floats with 2-byte precision
-        chn_i = int(header.decode('ascii')[-1]) + current_board * 4 
+
+    # Read and store data from a channel read from the header
+    elif header.startswith(b'C'):
+        chn_i = int(header.decode('ascii')[-1]) + current_board * 4 # channel number - 1
         scaler = unpack('I', f.read(4))
-        voltage_ints = unpack(b'H'*1024, f.read(2*1024))
+        voltage_ints = unpack(b'H'*1024, f.read(2*1024)) # the voltage info is 1024 floats with 2-byte precision
 
-        """
-        cellCalculate precise timing using the time bins and trigger
-        see p. 24 of the DRS4 manual for the explanation
-        the following lines sum up the times of all cells starting from the trigger cell
-        to the i_th cell and select only even members, because
-        the amplitude of the adjacent cells are averaged.
-        The width of the bins 1024-2047 is identical to the bins
-        0-1023, that is why the arrays are simply extended
-        before performing the cumsum operation
-        """
-        #timebins_full = list(roll(timebins[chn_i-1], -tcell))+ list(roll(timebins[chn_i-1], -tcell)) 
+        # Calculate precise timing using the time bins and trigger cell
+        # Sum up the times of all cells starting from the trigger cell to the i_th cell 
+        # and select only even members, as the amplitude of the adjacent cells are averaged.
         timebins_full = list(roll(timebins[chn_i-1], -tcell))+list(roll(timebins[chn_i-1], -tcell))
-        t = cumsum( timebins_full ) [ : : 2 ]
-
-        # time of first cell for correction , find the time of the first cell for each channel ,
-        # because only these cells are aligned in time
+        t = cumsum(timebins_full)[::2]
+        # Find the time of the first cell for alignment correction
         t_0 = t[(1024-tcell)%1024]
         if chn_i % 4 == 1:
             t_00 = t_0
         # Align all channels with the first channel
-        t = t - (t_0 - t_00) # correction with # TODO: it is a bit unclear how to do the correction
+        t = t - (t_0 - t_00) # correction
 
-        if chn_i in written_channels : 
-            channels_v_file = open(input_filename+"_Data/"+ input_filename+"_chn{}_v".format(chn_i), "a")
-            channels_t_file = open(input_filename+"_Data/"+ input_filename+"_chn{}_t".format(chn_i), "a")
-        else :
-            channels_v_file = open(input_filename+"_Data/"+ input_filename+"_chn{}_v".format(chn_i), "w")
-            channels_t_file = open(input_filename+"_Data/"+ input_filename+"_chn{}_t".format(chn_i), "w")
-            written_channels .append(chn_i)
-
-        for i , x in enumerate(voltage_ints):
+        # Output list of numpy arrays (to be plotted or used in the further analysis).
+        # Time vector per event: channels_t[channelnumber][cellnumber]
+        # Voltage vector per event: channels_v[channelnumber][cellnumber]
+        # NOTE: channels 0-3 are for the first board,  channels 4-7 are for the second board
+        for i, x in enumerate(voltage_ints): # i is the cell number, x is the voltage value of the sample in ADC channels
             channels_v[chn_i-1][i] = ((x / 65535.) - 0.5)
             channels_t[chn_i-1][i] = t[i]
-            channels_v_file.write(str ((x / 65535.) - 0.5))
-            channels_v_file.write(" ") # This is just to separate the different entries
-            channels_t_file.write(str(t[ i ]))
-            channels_t_file.write(" ") 
 
-        channels_v_file.write("\n") # In order to separate the different events
-        channels_t_file.write("\n")
-        channels_v_file.close()
-        channels_t_file.close()
-    
-    #triggertimelist =[]
-    elif (header == b""):
+        # Print times and voltages per event per channel
+        #print('  Channel: ',chn_i-1)
+        #print('    Times: ', channels_t[chn_i-1])
+        #print('    Voltages: ', channels_v[chn_i-1])
+
+    # End of File
+    elif header == b"":
         break
 
-print ("Done with event number ", current_event_n)
-print ("Your data is ready.")
 f.close()
+print('\n')
